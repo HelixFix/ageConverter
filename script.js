76c3957930b7027e0737718517a0e4d3a953dae2
HelixFix <jerome.rinner@gmail.com>
2020-01-31 16:35:48 +0100

    add space converter to all option

--- script.js
+++ script.js
@@ -6,40 +6,34 @@ console.log(button);
 
 let messageDoyen = "Que je sache la personne la plus âgée ayant existé a ce jour avait 121 ans";
 let messageBirth = "Hello World"
-let messageEgg= "Qu'est-ce qui est apparu en premier : l'œuf ou la poule ?"
-
-
-
-
-
+let messageEgg = "Qu'est-ce qui est apparu en premier : l'œuf ou la poule ?"
 
 document.getElementById("button").addEventListener("click", function (e) {
     e.preventDefault();
 
-    
 
     ageEntrer = document.getElementById('age').value;
 
     //Secondes
-    if (document.getElementById('inputSecondes').checked){
-        if (ageEntrer < 122 && ageEntrer > 0 ){
+    if (document.getElementById('inputSecondes').checked) {
+        if (ageEntrer < 122 && ageEntrer > 0) {
             let secondsAge = ageEntrer * 60 * 60 * 24 * 365;
             let ageSpace = secondsAge.toLocaleString()
 
             console.log(ageSpace);
-            let age = document.getElementById("result").innerHTML = ageSpace;
-               
-           
+            let age = document.getElementById("result").innerHTML = "Vous avez " + ageSpace + " secondes.";
+
+
         }
 
-        if (ageEntrer == 0 ){
+        if (ageEntrer == 0) {
             let age = document.getElementById("result").innerHTML = messageBirth
         }
         if (ageEntrer > 121) {
             let age = document.getElementById("result").innerHTML = messageDoyen
         }
 
-        if (ageEntrer < 0 ) {
+        if (ageEntrer < 0) {
             let age = document.getElementById("result").innerHTML = messageEgg
         }
     }
@@ -47,20 +41,33 @@ document.getElementById("button").addEventListener("click", function (e) {
 
     //Minutes
     if (document.getElementById('inputMinutes').checked) {
-        
-        let age = document.getElementById("result").innerHTML = ageEntrer * 60 * 24 * 365;
+        let minutesAge = ageEntrer * 60 * 24 * 365;
+        let ageSpace = minutesAge.toLocaleString()
+
+        console.log(ageSpace);
+        let age = document.getElementById("result").innerHTML = "Vous avez " + ageSpace + " minutes.";
+
+
     }
 
     //heures
     if (document.getElementById('inputHeures').checked) {
-        
-        let age = document.getElementById("result").innerHTML = ageEntrer * 24 * 365;
+        let hourAge = ageEntrer * 24 * 365;
+        let ageSpace = hourAge.toLocaleString()
+
+        console.log(ageSpace);
+        let age = document.getElementById("result").innerHTML = "Vous avez " + ageSpace + " heures.";
+
     }
 
     //jours
     if (document.getElementById('inputJours').checked) {
-        
-        let age = document.getElementById("result").innerHTML = ageEntrer * 365;
+        let daysAge = ageEntrer * 365;
+        let ageSpace = daysAge.toLocaleString()
+
+        console.log(ageSpace);
+        let age = document.getElementById("result").innerHTML = "Vous avez " + ageSpace + " jours.";
+
     }
 
 });
